ebb8cccdee4c3b7e0d01816db20ed90da577ab24
thasanee <meow3525@gmail.com>
2022-03-12 15:34:38 -0600

    add all file

---
+++ challenges/frequency-counter-same-frequency.js
@@ -0,0 +1,26 @@
+/* 
+Input : number
+Output : boolean
+
+*/
+
+function sameFrequency(num1, num2) {
+    // good luck. Add any arguments you deem necessary.
+    /*
+    A(input: number)
+        converse both numbers to string 
+        set {} to lookup at key and value to check  if both numbers are have same value
+        loop through num1 to find key and set value 1 to lookup for counting
+            if 
+
+        
+        B(output: boolean)
+      
+    */
+
+
+    return true
+}
+
+// console.log('1 :', sameFrequency(182, 281)) //  true
+console.log('--:', sameFrequency(182, 282)) //  false
