efceca148743ac53e5a66ee8173b7d83d03a74ab
Meowmerry <meow3525@gmail.com>
2022-04-11 06:07:33 -0500

    linkedList refactor

--- Linked-Lists-Data-structuresJs/singly-linked-lists/exercise/Singly_Linked_List.js
+++ Linked-Lists-Data-structuresJs/singly-linked-lists/exercise/Singly_Linked_List.js
@@ -8,43 +8,25 @@ class SingleLinkedList {
     constructor() {
         this.head = null;
         this.tail = null;
-        this.length = null;
+        this.length = 0;
     }
     push(val) {
         let newNode = new Node(val);
         if (!this.head) {
             this.head = newNode;
-            this.tail = this.head
+            this.tail = this.head;
         } else {
             this.tail.next = newNode;
-            this.tail = newNode
+            this.tail = newNode;
         }
         this.length++;
         return this;
     }
-    pop() {
-        let currentHead = this.head;
-        if (!this.head) return undefined;
-
-        let current = this.head;
-        let newTail = current;
-        while (current.next) {
-            newTail = current;
-            current = current.next
-        }
-        this.tail = newTail;
-        this.tail.next = null;
-        this.length--;
-        if (this.length === 0) {
-            this.head = null;
-            this.tail = null;
-        }
-        return current;
-    }
 }
-const lists = new SingleLinkedList()
-lists.push(10);
-lists.push(20);
-lists.push(30);
-lists.pop()
-console.log(lists)
+
+const list = new SingleLinkedList()
+list.push(10);
+list.push(20);
+list.push(30);
+//list.pop()
+console.log(list)
